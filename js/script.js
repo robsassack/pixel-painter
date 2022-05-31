f4edfe0d18f776eabdb8c6dd16c5f95ef877181b
Rob Sassack <rsassack25@gmail.com>
2022-05-31 15:11:24 -0400

    Pixel grid is now resizable

--- js/script.js
+++ js/script.js
@@ -1,29 +1,45 @@
+// options menu
+let slider = document.querySelector(".slider");
+let sizeText = document.querySelector(".size-text");
+slider.oninput = function () {
+  sizeText.textContent = `${slider.value}x${slider.value}`;
+  createDrawingBoard();
+//   drawable();
+};
+
 // create drawing boward
-const drawingBoard = document.querySelector('.drawing-board');
-for (let x=0; x<16; x++){
+function createDrawingBoard() {
+  const drawingBoard = document.querySelector(".drawing-board");
+  // clear the grid in case a different size is selected
+  while (drawingBoard.firstChild) {
+      drawingBoard.removeChild(drawingBoard.firstChild);
+  }
+  for (let x = 0; x<slider.value; x++) {
     // create column of pixels to paint
-    const gridColumn = document.createElement('div');
-    gridColumn.classList.add('grid-column');
-    for (let y=0; y<16; y++){
-        // create each pixel for the column
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridColumn.appendChild(gridItem);
+    const gridColumn = document.createElement("div");
+    gridColumn.classList.add("grid-column");
+    for (let y = 0; y<slider.value; y++) {
+      // create each pixel for the column
+      const gridItem = document.createElement("div");
+      gridItem.classList.add("grid-item");
+      gridColumn.appendChild(gridItem);
     }
     drawingBoard.appendChild(gridColumn);
-}
+  }
 
-// select the grid to make them paintable
-const paintItem = document.querySelectorAll('.grid-item');
-for (let i=0; i<paintItem.length; i++) {
+  // select the grid to make the pixels paintable
+  const paintItem = document.querySelectorAll(".grid-item");
+  for (let i = 0; i < paintItem.length; i++) {
     // make it so clicking paints the pxiel
-    paintItem[i].addEventListener('mousedown', e => {
-        paintItem[i].style.backgroundColor = 'black';
-    })
+    paintItem[i].addEventListener("mousedown", (e) => {
+      paintItem[i].style.backgroundColor = "black";
+    });
     // also makes it possible to click and drag
-    paintItem[i].addEventListener('mousemove', e=> {
-        if (e.buttons === 1) {
-            paintItem[i].style.backgroundColor = 'black';
-        }
-    })
+    paintItem[i].addEventListener("mousemove", (e) => {
+      if (e.buttons === 1) {
+        paintItem[i].style.backgroundColor = "black";
+      }
+    });
+  }
 }
+createDrawingBoard();
